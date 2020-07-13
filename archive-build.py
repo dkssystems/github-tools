21f77a3acea292b619557b9de64d2ceb293ae2b4
Eric Schneider <eschneider@dkssystems.com>
2020-07-12 20:23:23 -0500

    add archive scripts

---
+++ archive-build.py
@@ -0,0 +1,77 @@
+import sys, os
+import zipfile, posixpath
+import argparse
+import pathspec
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('--archive', action='store', dest='archive', required=True, help='Filename of archive created')
+parser.add_argument('--local', action='store', dest='local_root', required=True, help='Local root folder')
+parser.add_argument('--dry-run', action='store_true', dest='dry_run', default=False, help='Output only (no changes)')
+
+args = parser.parse_args()
+
+if not os.path.exists(args.local_root):
+    os.makedirs(args.local_root)
+
+ignore = None
+ignore_filename = os.path.join(args.local_root, '.pushignore')
+if os.path.exists(ignore_filename):
+    with open(ignore_filename) as ignore_file:
+        ignore = pathspec.PathSpec.from_lines(pathspec.patterns.GitWildMatchPattern, ignore_file.readlines())
+
+def apply_ignores(list):
+    if ignore is None:
+        return list
+    
+    for item in list:
+        file = item['archive_path']
+        if ignore.match_file(file):
+            print(file)
+        else:
+            yield item
+
+def walk_error(err):
+    print (err)
+    exit(1)
+
+# Check every item and build copy list
+print ("WALKING DIRECTORY")
+copies = []
+
+local_root = args.local_root.replace('\\', '/').strip().rstrip('/')
+
+for local_parent, child_dirs, child_files in os.walk(local_root, onerror=walk_error):
+    if '.git' in child_dirs:
+        child_dirs.remove('.git') # ignore .git directories
+        
+    archive_parent = str(local_parent[(len(local_root)+1)::].replace(os.sep, posixpath.sep)) # Turn local to archive parent
+    for child_file in child_files:
+        local_child_path = os.path.join(local_parent, child_file)
+        archive_child_path = posixpath.join(archive_parent, child_file)
+        copies.append({'local_path': local_child_path, 'archive_path': archive_child_path})
+
+# Apply .pushignore
+if not ignore is None:
+    print ("APPLYING .PUSHIGNORE RULES")
+    copies = list(apply_ignores(copies))
+
+if not args.dry_run:
+    with zipfile.ZipFile(args.archive, 'w') as zip:
+        # Push ignore
+        if not ignore is None:
+            zip.write(ignore_filename, '.pushignore')
+
+        # Perform copies
+        print ("COPY LIST")
+
+        copies_meta = []
+        for copy in copies:
+            print (copy['local_path'])
+            print (" --> " + copy['archive_path'])
+            copies_meta.append(copy['archive_path'])
+            zip.write(copy['local_path'], posixpath.join('copies', copy['archive_path']))
+        
+        zip.writestr('copies.txt', '\n'.join(copies_meta))
+
+print ("DONE")
---
+++ archive-deploy.py
@@ -0,0 +1,163 @@
+#!/usr/bin/env python3
+import sys, os, io
+import zipfile, posixpath, shutil
+import argparse
+import pathspec
+import zlib
+import requests
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('--archive-url', action='store', dest='archive_url', required=True, help='Archive file URL')
+parser.add_argument('--remote', action='store', dest='remote_root', required=True, help='Remote root folder')
+parser.add_argument('--artifacts', action='store', dest='artifacts_root', required=False, help='Artifacts (backup) root folder')
+parser.add_argument('--dry-run', action='store_true', dest='dry_run', default=False, help='Output only (no changes)')
+
+args = parser.parse_args()
+
+def crc32(filename):
+    if not os.path.exists(filename):
+        return -1
+
+    fh = open(filename, 'rb')
+    hash = 0
+    while True:
+        s = fh.read(65536)
+        if not s:
+            break
+        hash = zlib.crc32(s, hash)
+    fh.close()
+    return (hash & 0xFFFFFFFF)
+
+def file_ignored(file):
+    if ignore is None:
+        return False
+
+    return ignore.match_file(file)
+
+def read_list(zip, filename, default = []):
+    try:
+        with zip.open(filename, 'r') as file:
+            return [line.decode('utf-8').rstrip('\n') for line in file.readlines()]
+    except KeyError:
+        return default
+
+def walk_error(err):
+    print (err)
+    exit(1)
+
+def copy_zip_file(zip, archive_path, remote_path):
+    print ('DEPLOYING ' + archive_path)
+    print (" --> " + remote_path)
+
+    if not args.dry_run:
+        remote_dir = os.path.dirname(remote_path)
+        if not os.path.exists(remote_dir):
+            os.makedirs(remote_dir)
+
+        # We can't use ZipFile.extract because it preserves the archive path
+        source = zip.open(posixpath.join('copies', archive_path))
+        target = open(remote_path, "wb")
+        with source, target:
+            shutil.copyfileobj(source, target)
+
+def artifact_file(from_path, to_path):
+    print ('ARTIFACT ' + from_path)
+    print (" --> " + to_path)
+
+    if not args.dry_run:
+        to_dir = os.path.dirname(to_path)
+        if not os.path.exists(to_dir):
+            os.makedirs(to_dir)
+
+        shutil.move(from_path, to_path)
+
+print ("DOWNLOADING ARCHIVE")
+response = requests.get(args.archive_url)
+
+with zipfile.ZipFile(io.BytesIO(response.content)) as zip:
+    if zip.testzip() is not None:
+        print ('ERROR: archive failed CRC test')
+        exit()
+
+    # Read ignores
+    ignore = None
+    ignores = read_list(zip, '.pushignore', None)
+    if ignores is not None:
+        ignore = pathspec.PathSpec.from_lines(pathspec.patterns.GitWildMatchPattern, ignores)
+            
+    # Read through remote files and check for differences
+    print ("WALKING DIRECTORY")
+
+    # Create hash of files in archive. Files will be removed which match a remote file.
+    archive_hash = {}
+    for archive_path in read_list(zip, 'copies.txt'):
+        archive_hash[archive_path] = None
+
+    remotes = []
+
+    remote_root = args.remote_root.replace('\\', '/').strip().rstrip('/').replace(posixpath.sep, os.sep)
+    artifacts_root = (args.artifacts_root or '/deployment-artifacts').replace('\\', '/').strip().rstrip('/').replace(posixpath.sep, os.sep)
+
+    if not os.path.exists(remote_root):
+        os.makedirs(remote_root)
+
+    for remote_parent, child_dirs, child_files in os.walk(remote_root, onerror=walk_error):
+        rel_parent = str(remote_parent[(len(remote_root)+1)::])
+        archive_parent = rel_parent.replace(os.sep, posixpath.sep) # Turn remote to archive parent
+        artifacts_parent = os.path.join(artifacts_root, rel_parent)
+        
+        # Directories
+        for child_dir in child_dirs:
+            archive_child_path = posixpath.join(archive_parent, child_dir)
+            
+            # Check if directory should be ignored
+            if file_ignored(archive_child_path + posixpath.sep):
+                child_dirs.remove(child_dir)
+
+        # Files
+        for child_file in child_files:
+            archive_child_path = posixpath.join(archive_parent, child_file)
+            remote_child_path = os.path.join(remote_parent, child_file)
+            artifacts_child_path = os.path.join(artifacts_parent, child_file)
+
+            # Check if file should be ignored
+            if file_ignored(archive_child_path):
+                continue
+
+            if archive_child_path in archive_hash:
+                # File should exist
+                archive_info = zip.getinfo(posixpath.join('copies', archive_child_path))
+                remote_info = None
+                if os.path.exists(remote_child_path):
+                    remote_info = os.stat(remote_child_path)
+
+                changed = False
+
+                # First check for file size difference
+                if remote_info is None or remote_info.st_size != archive_info.file_size:
+                    changed = True
+                
+                # Then check for hash difference
+                if not changed:
+                    remote_crc = crc32(remote_child_path)
+                    if archive_info.CRC != remote_crc:
+                        changed = True
+
+                if changed:
+                    # File should be deployed
+                    artifact_file(remote_child_path, artifacts_child_path)
+                    copy_zip_file(zip, archive_child_path, remote_child_path)
+
+                del archive_hash[archive_child_path]
+            else:
+                # File should not exist
+                artifact_file(remote_child_path, artifacts_child_path)
+    
+    print ('NEW FILES')
+    # Files remaining in hash are new
+    for archive_path in archive_hash.keys():
+        remote_path = os.path.join(remote_root, archive_path.replace(posixpath.sep, os.sep)) # Turn archive to remote parent
+        copy_zip_file(zip, archive_path, remote_path)
+
+print ("DONE")
